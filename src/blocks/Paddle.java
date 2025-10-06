d246a3029bd3d67fc42d4bbc356fd9444d1b09bc
pfaj <pfaj2004@gmail.com>
2025-10-06 10:57:50 -0400

    blocks: added functionality to have either images or color as the main way to display the blocks

--- src/blocks/Paddle.java
+++ src/blocks/Paddle.java
@@ -1,18 +1,38 @@
 package blocks;
 
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+
 public class Paddle extends Block {
 	private static final int MOVE_SPEED = 15;
 
-	public Paddle(int xPosition, int yPosition, int width, int height) {
-		super(xPosition, yPosition, width, height);
+//	public Paddle(int xPosition, int yPosition, int width, int height) {
+//		super(xPosition, yPosition, width, height);
+//	}
+
+	/*
+	 * Paddle constructor using a color fill
+	 */
+	public Paddle(Color color, int xPosition, int yPosition, int width, int height) {
+		super(color, xPosition, yPosition, width, height );
 	}
 
+	/*
+	 * Paddle constructor using an image fill
+	 */
+	public Paddle(int xPosition, int yPosition, int width, int height, Image image) {
+		super(xPosition, yPosition, width, height, image);
+	}
+
+	/*
+	 * Provides paddle movement which the user can use to control
+	 */
 	public void moveHorizontally(boolean goRight) {
 		if (goRight)
-			rect.setX(rect.getX() - MOVE_SPEED);
+			this.setX(this.getX() - MOVE_SPEED);
 
 		else {
-			rect.setX(rect.getX() + MOVE_SPEED);
+			this.setX(this.getX() + MOVE_SPEED);
 		}
 	}
 
