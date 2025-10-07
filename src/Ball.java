c097a5bfec0a99f4fecd00250bf325221221d667
pfaj <pfaj2004@gmail.com>
2025-10-06 23:54:17 -0400

    ball: changed the way that the physics are done and added a max/min velocity to the ball

--- src/Ball.java
+++ src/Ball.java
@@ -13,11 +13,16 @@ public class Ball {
     private Point2D position;
     private final Circle myBall; 
     private Point2D previousLocation;
+    
+    private static final double FRICTION_FACTOR = 0.99;
+    private static final int MAX_VELOCITY = 500;
+    private static final int MIN_VELOCITY = -MAX_VELOCITY;
+
 
 
     public Ball() {
-        this.velocity = new Point2D(150, 150); 
-        this.myBall = new Circle(10.0, Color.BLUE); 
+        this.velocity = new Point2D(175, -150); 
+        this.myBall = new Circle(10.0, Color.RED); 
         // TODO: Change to non default cords
         // being used for testing purposes
         this.myBall.setCenterX(200);
@@ -68,19 +73,35 @@ public class Ball {
         bounce(isReflectingXAxis, 1.0);
     }
 
+    public void stop() {
+        velocity = new Point2D(0,0);
+    }
+
     /*
      * This method reverses the ball's velocity while also
      * changing it by a factor 'modification'.
      */
     public void bounce(boolean isReflectingXAxis, double modification) {
+    	double deltaX = velocity.getX();
+    	double deltaY = velocity.getY();
+    	if(deltaY > MAX_VELOCITY){
+    		deltaY = MAX_VELOCITY;
+    	}
+
+    	if(0 > deltaY  && deltaY < MIN_VELOCITY) {
+    		deltaY = MIN_VELOCITY;
+    	}
+
         if (isReflectingXAxis) {
             // Create a new Point2D object for the new velocity
-            velocity = new Point2D(velocity.getX() * -modification, velocity.getY());
+            velocity = new Point2D(deltaX * -modification , (deltaY + getRandomDouble(0,1)));
+			myBall.setCenterX(previousLocation.getX() - (myBall.getRadius()/3));
+			myBall.setCenterY(previousLocation.getY());
         } else {
-            velocity = new Point2D(velocity.getX(), velocity.getY() * -modification);
+            velocity = new Point2D(deltaX + getRandomDouble(0,1), deltaY * -modification);
+			myBall.setCenterX(previousLocation.getX());
+			myBall.setCenterY(previousLocation.getY() - (myBall.getRadius()/3));
         }
-        myBall.setCenterX(previousLocation.getX() - 1);
-        myBall.setCenterY(previousLocation.getY() - 1);
     }
 
 
@@ -88,29 +109,46 @@ public class Ball {
      * This method will change the ball velocity and cause it to bounce off the display wall
      */
     public void bounceOffWall(double screenWidth, double screenHeight) {
-    	if (myBall.getCenterX() - myBall.getRadius() < 0 || myBall.getCenterX() + myBall.getRadius() > screenWidth - myBall.getBoundsInLocal().getWidth()) {
-    		velocity = new Point2D(-velocity.getX(), velocity.getY());
-    	}
-    	if (myBall.getCenterY() - myBall.getRadius() < 0 || (myBall.getCenterY()+ myBall.getRadius()) > screenHeight - myBall.getBoundsInLocal().getHeight()) {
-    		velocity = new Point2D(velocity.getX(), -velocity.getY());
-    	}
-    }
+        // Get ball properties 
+        double radius = myBall.getRadius();
+        double x = myBall.getCenterX();
+        double y = myBall.getCenterY();
 
+        // Horizontal (X-axis) collision
+        if (x - radius < 0) {
+            myBall.setCenterX(radius); 
+            velocity = new Point2D(-velocity.getX(), velocity.getY());
+        } else if (x + radius > screenWidth) {
+            velocity = new Point2D(-velocity.getX(), velocity.getY());
+        }
+
+        // Vertical (Y-axis) collision
+        if (y - radius < 0) {
+            velocity = new Point2D(velocity.getX(), -velocity.getY());
+        } else if (y + radius > screenHeight) {
+            velocity = new Point2D(velocity.getX(), -velocity.getY());
+        }
+        if (y - radius < 0 && x - radius < 0) {
+            myBall.setCenterX(-radius); 
+            myBall.setCenterY(-radius); 
+            velocity = new Point2D(velocity.getX(), velocity.getY());
+        }
+    }
     /*
      * Updates the ball's position 
      */
     public void move(double elapsedTime) {
     	// Calculate the new position: newPosition = oldPosition + velocity * time
     	previousLocation = new Point2D(myBall.getCenterX(), myBall.getCenterY());
-    	double newX = myBall.getCenterX() + velocity.getX() * elapsedTime;
-    	double newY = myBall.getCenterY() + velocity.getY() * elapsedTime;
+    	double newX = (myBall.getCenterX() + (velocity.getX() * FRICTION_FACTOR)* elapsedTime);
+    	double newY = (myBall.getCenterY() + (velocity.getY() * FRICTION_FACTOR)* elapsedTime);
 
     	// Update the circle's position
     	myBall.setCenterX(newX);
     	myBall.setCenterY(newY);
     }
     
-    private int getRandomInt(int rangeStart, int rangeEnd) {
-    	return rangeStart + random.nextInt(rangeEnd - rangeStart) + 1;
+    private double getRandomDouble(double rangeStart, double rangeEnd) {
+    	return rangeStart + random.nextDouble(rangeEnd - rangeStart) + 1;
     }
 }
