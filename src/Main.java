68f93d4cc8b7e560705593f5740373ea7393f587
pfaj <pfaj2004@gmail.com>
2025-10-06 23:54:38 -0400

    main: changed the size of the display and the title of the project

--- src/Main.java
+++ src/Main.java
@@ -19,11 +19,12 @@ import javafx.util.Duration;
  */
 public class Main extends Application {
 
-	public static final int SIZE = 400;
+	public static final int HEIGHT = 600;
+	public static final int WIDTH= 400;
 	public static final int FRAMES_PER_SECOND = 60;
 	public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
 	public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
-	public static final String TITLE = "Example JavaFX";
+	public static final String TITLE = "OBW Breakout";
     public static final Paint BACKGROUND = Color.AZURE;
 
 
@@ -40,7 +41,7 @@ public class Main extends Application {
 	public void start (Stage stage) {
 		myAnimation = new AnimationController();
 		// attach scene to the stage and display it
-		myScene = setupScene(SIZE, SIZE, BACKGROUND);
+		myScene = setupScene(WIDTH, HEIGHT, BACKGROUND);
 		stage.setScene(myScene);
 		stage.setTitle(TITLE);
 		stage.show();
@@ -78,10 +79,10 @@ public class Main extends Application {
 //			myAnimation.moverMovesVertically(false);
 //		}
 		if (code == KeyCode.LEFT) {
-			myAnimation.moverMovesHorizontally(true);
+			myAnimation.paddleMovesRight(false);
 		}
 		else if (code  == KeyCode.RIGHT) {
-			myAnimation.moverMovesHorizontally(false);
+			myAnimation.paddleMovesRight(true);
 			
 		}
 	}
