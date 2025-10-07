b153dac4a67374f97917fb92d84a7d37e89bc2c6
pfaj <pfaj2004@gmail.com>
2025-10-06 23:52:11 -0400

    paddle: added movement limits based on the board width

--- src/blocks/Paddle.java
+++ src/blocks/Paddle.java
@@ -5,6 +5,9 @@ import javafx.scene.paint.Color;
 
 public class Paddle extends Block {
 	private static final int MOVE_SPEED = 15;
+	private boolean moved = false;
+	private int boardWidth;
+	private int paddleWidth;
 
 //	public Paddle(int xPosition, int yPosition, int width, int height) {
 //		super(xPosition, yPosition, width, height);
@@ -13,27 +16,37 @@ public class Paddle extends Block {
 	/*
 	 * Paddle constructor using a color fill
 	 */
-	public Paddle(Color color, int xPosition, int yPosition, int width, int height) {
+	public Paddle(Color color, int xPosition, int yPosition, int width, int height, int boardWidth) {
 		super(color, xPosition, yPosition, width, height );
+		this.boardWidth = boardWidth;
+		this.paddleWidth = width;
 	}
 
 	/*
 	 * Paddle constructor using an image fill
 	 */
-	public Paddle(int xPosition, int yPosition, int width, int height, Image image) {
+	public Paddle(int xPosition, int yPosition, int width, int height, int  boardWidth,Image image) {
 		super(xPosition, yPosition, width, height, image);
+		this.boardWidth = boardWidth;
+		this.paddleWidth = width;
 	}
 
 	/*
 	 * Provides paddle movement which the user can use to control
 	 */
 	public void moveHorizontally(boolean goRight) {
-		if (goRight)
-			this.setX(this.getX() - MOVE_SPEED);
-
-		else {
-			this.setX(this.getX() + MOVE_SPEED);
+		if(!moved) {
+			moved = true;
 		}
+		if (goRight && this.getView().getLayoutX() + paddleWidth < boardWidth) {
+		    this.setX(this.getX() + MOVE_SPEED);
+		} else if (!goRight && this.getView().getLayoutX() > 0) {
+		    this.setX(this.getX() - MOVE_SPEED);
+		}
+	}
+
+	public boolean hasBeenMoved() {
+		return moved;
 	}
 
 }
