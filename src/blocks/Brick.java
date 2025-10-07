7462d9ed1d12b937401d8e6d92719f444a3b8156
pfaj <pfaj2004@gmail.com>
2025-10-06 23:52:45 -0400

    brick: added points, durability, and speed multiplier to the constructor

--- src/blocks/Brick.java
+++ src/blocks/Brick.java
@@ -1,19 +1,37 @@
 package blocks;
 
 public class Brick extends Block{
+	private static final int BASE_MULTIPLIER = 2;
+	private static final int BASE_POINTS = 10;
+	private static final int BASE_DURABILITY = 1;
+
 	private double hitForceMultiplier;
 	private int points;
+	private int durability;
 
-	public Brick(int xPosition, int yPosition, int width, int height, double hitForceMultiplier, int points) {
+	public Brick(int xPosition, int yPosition, int width, int height, double hitForceMultiplier, int points, int durability) {
 		super(xPosition, yPosition, width, height);
 		this.hitForceMultiplier = hitForceMultiplier;
 		this.points = points;
+		this.durability = durability;
 	}
 
 	public Brick(int xPosition, int yPosition, int width, int height) {
 		super(xPosition, yPosition, width, height);
-		this.hitForceMultiplier = 2;
-		this.points = 10;
+		this.hitForceMultiplier = BASE_MULTIPLIER;
+		this.points = BASE_POINTS;
+		this.durability = BASE_DURABILITY;
+	}
+	
+	public void removeDurability() {
+		durability -=1;
 	}
 
+	public boolean isBroken() {
+		return durability <= 1;
+	}
+	
+	public double getHitForceMultiplier() {
+		return hitForceMultiplier;
+	}
 }
