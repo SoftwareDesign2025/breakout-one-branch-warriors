d0cc8d8ba1ed8119b86a0bf65075380fe44d893b
Spoon <aspoerndle@elon.edu>
2025-10-21 17:57:33 -0400

    Cleaned up main method and adjusted logic to HighScoreController

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,11 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 /**
- * A basic example JavaFX program for the first lab.
+ * @Author OneBranchWarriors
  * 
- * @author Shannon Duvall
  * 
- * This class contains the code that runs the basic window.
+ *         This class contains the code to begin the program and define the
+ *         Scene
  * 
  */
 public class Main extends Application {
@@ -25,27 +25,29 @@ public class Main extends Application {
 	public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
 	public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
 	public static final String TITLE = "OBW Breakout";
-    public static final Paint BACKGROUND = Color.AZURE;
-
+	public static final Paint BACKGROUND = Color.AZURE;
 
 	private Scene myScene;
-	
+
 	// This class will handle the logic that is specific to this application.
 	private AnimationController myAnimation;
 
 	
-	/**
-	 * Initialize what will be displayed and how it will be updated.
-	 */
+	// Input: stage
+	// Output: None
+	// Purpose: begins the program using predefined constants such as screen
+	// dimensions, background color, and time delay for each frame
+
 	@Override
-	public void start (Stage stage) {
+	public void start(Stage stage) {
 		myAnimation = new AnimationController();
 		// attach scene to the stage and display it
 		myScene = setupScene(WIDTH, HEIGHT, BACKGROUND);
 		stage.setScene(myScene);
 		stage.setTitle(TITLE);
 		stage.show();
-		// attach "game loop" to timeline to play it (basically just calling step() method repeatedly forever)
+		// attach "game loop" to timeline to play it (basically just calling step()
+		// method repeatedly forever)
 		KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step(SECOND_DELAY));
 		Timeline animation = new Timeline();
 		animation.setCycleCount(Timeline.INDEFINITE);
@@ -53,53 +55,55 @@ public class Main extends Application {
 		animation.play();
 	}
 
-	// Create the "scene": what shapes will be drawn and their starting properties
-	private Scene setupScene (int width, int height, Paint background) {
-		Group root = myAnimation.createRootForAnimation(width, height);
+	// Input: screen width, screen height, background color
+	// Output: Scene
+	// Purpose: creates the "Scene" including what shapes will be drawn and their
+	// starting properties
+	private Scene setupScene(int WIDTH, int HEIGHT, Paint background) {
+		Group root = myAnimation.createRootForAnimation(WIDTH, HEIGHT);
 		// create a place to see the shapes
-		Scene scene = new Scene(root, width, height, background);
+		Scene scene = new Scene(root, WIDTH, HEIGHT, background);
 		// respond to input
 		scene.setOnKeyReleased(e -> stopPaddle());
 		scene.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
-//		scene.setOnMouseClicked(e -> handleMouseInput(e.getX(), e.getY()));
+
 		return scene;
 	}
 
-	// This is the method that gets called over and over and over...
-	// every 15 milliseconds or so!
-	private void step (double elapsedTime) {
+	// Input: elapsedTime
+	// Output: None
+	// Purpose: uses the defined step method from the animation controller to
+	// determine the necessary time delay for each frame
+
+	private void step(double elapsedTime) {
 		myAnimation.step(elapsedTime);
 	}
 
-	// What to do each time a key is pressed
-	private void handleKeyInput (KeyCode code) {
-//		if (code == KeyCode.UP) {
-//			myAnimation.moverMovesVertically(true);
-//		}
-//		else if (code == KeyCode.DOWN) {
-//			myAnimation.moverMovesVertically(false);
-//		}
+	// Input: KeyCode
+	// Output: none
+	// Purpose: determines what animation needs to be done depending on the key
+	// pressed
+	private void handleKeyInput(KeyCode code) {
+
 		if (code == KeyCode.LEFT) {
 			myAnimation.paddleMovesRight(false);
-		}
-		else if (code  == KeyCode.RIGHT) {
+		} else if (code == KeyCode.RIGHT) {
 			myAnimation.paddleMovesRight(true);
-			
+
 		}
 	}
+
+	// Input: none
+	// Output: none
+	// Purpose: stops the paddle from moving
 	private void stopPaddle() {
 		myAnimation.stopPaddle();
 	}
 
-	// What to do each time a key is pressed
-//	private void handleMouseInput (double x, double y) {
-//		myAnimation.handleMouseInput(x, y);
-//	}
-
 	/**
 	 * Start the program.
 	 */
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		launch(args);
 	}
 }
